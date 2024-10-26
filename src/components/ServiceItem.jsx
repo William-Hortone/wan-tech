fefb61058dd8e548fd55dbd3665f091ce7ffbdaf
William Hortone <101454117+William-Hortone@users.noreply.github.com>
2024-10-27 02:30:20 +0800

    updated the home service section

--- src/components/ServiceItem.jsx
+++ src/components/ServiceItem.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 
-const ServiceItem = () => {
+const ServiceItem = ({ title, text, img, bgColor, color, className }) => {
   return (
-    <>
-      <div className="w-full h-24 bg-orange-700">ServiceItem</div>
-    </>
+    <div className={`w-full h-auto 2xl:min-h-64 bg-orange-700 flex flex-col justify-between p-4 md:p-8 md:flex-row ${className}`} style={{ backgroundColor: bgColor }}>
+      <div className="flex flex-col gap-4 w-full md:w-2/4">
+        <h2 className="font-fontBase text-start font-bold text-2xl py-2 lg:text-4xl" style={{ color: color }}>{title}</h2>
+        <p className="font-opensans text-start text-lg" style={{ color: color }}>{text}</p>
+      </div>
+      <div className="w-full min-h-44 md:w-1/4 bg-sky-800 ">
+        <img src={img} alt="project pic" />
+      </div>
+    </div>
   );
 };
 
--- src/containers/homeContainers/ProjectView.jsx
+++ src/containers/homeContainers/ProjectView.jsx
@@ -1,13 +1,66 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { ServiceItem, Title } from "../../components";
 
+gsap.registerPlugin(ScrollTrigger);
+
 const ProjectView = () => {
+  useEffect(() => {
+    const serviceItems = gsap.utils.toArray(".service-item");
+    
+    //  Animation of each service item
+    serviceItems.forEach((item, index) => {
+      gsap.fromTo(
+        item,
+        { y: 100, opacity: 0 },
+        {
+          y: -80 * index,
+          opacity: 1,
+          scrollTrigger: {
+            trigger: item,
+            start: "top 80%",
+            end: "top 35%",
+            scrub: true,
+          },
+        }
+      );
+    });
+  }, []);
+
   return (
     <>
-      <section className="w-full h-screen bg-orange-600 p-8">
-        <Title title="Sevices" />
+      <section className="w-full h-auto bg-primary pb-8">
+        <div className="p-8">
+          <Title title="Services" />
+        </div>
 
-        <ServiceItem />
+        <div className="w-full h-auto flex flex-col">
+          <ServiceItem
+            title="SITES WEB"
+            text="Nous concevons des sites web sur mesure qui reflètent l'identité de votre marque et répondent à vos objectifs commerciaux. Que vous ayez besoin d'un site vitrine, d'une plateforme e-commerce ou d'un blog,"
+            img=""
+            bgColor="#1E1E1E"
+            color="#ffffff"
+            className="service-item"
+          />
+          <ServiceItem
+            title="APPLICATIONS MOBILE"
+            text="Nous concevons des applications mobiles innovantes, parfaitement adaptées aux besoins uniques de votre entreprise et de vos utilisateurs. Nous créons des applications intuitives, performantes et visuellement attrayantes, qu'elles soient destinées à iOS, Android ou aux deux."
+            img=""
+            bgColor="#C3ABFF"
+            color="#ffffff"
+            className="service-item"
+          />
+          <ServiceItem
+            title="LOGO & AFFICHES"
+            text="Votre image de marque commence par un logo percutant et des affiches attrayantes. Nous travaillons en étroite collaboration avec vous pour comprendre vos valeurs et votre vision, afin de créer des éléments graphiques qui captivent votre audience."
+            img=""
+            bgColor="#D9D9D9"
+            color="#000000"
+            className="service-item"
+          />
+        </div>
       </section>
     </>
   );
