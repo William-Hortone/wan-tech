66ca176d6b95e797abdf46b9fb4f1c474890811e
William Hortone <101454117+William-Hortone@users.noreply.github.com>
2025-01-10 20:11:26 +0800

    updated the header

--- src/containers/homeContainers/Header.jsx
+++ src/containers/homeContainers/Header.jsx
@@ -9,52 +9,10 @@ const Header = ({setShowMenu,showMenu}) => {
         <Infos />
         <NavBtn setShowMenu={setShowMenu} />
         <NavBar setShowMenu={setShowMenu} showMenu={showMenu} />
-        {/* Logo */}
-        {/* <div className="flex items-center justify-between w-full h-16 px-0 lg:px-16 ">
-          <button
-            className="flex items-center gap-2 rounded-sm cursor-pointer app__button group"
- 
-          >
-            <div
-              className="flex items-center w-[130px] md:w-[200px]  justify-start app__button-content bg-basic"
-         
-            >
-              <p className="whitespace-nowrap " style={{ color: "white" }}>
-                WAN TECH
-              </p>
-              <p className="whitespace-nowrap " style={{ color: "white" }}>
-                WAN TECH{" "}
-              </p>
-            </div>
-          </button>
-         
-
-          <button
-            onClick={() => setShowMenu(true)}
-            className="flex items-center gap-2 p-4 rounded-sm cursor-pointer app__button group"
-            style={{ }}
-          >
-            <div className="flex w-[130px] md:w-[200px] items-center  app__button-content bg-basic">
-              <FaRegEye
-                size={20}
-                color="black"
-                className="block transition-all duration-300 ease-in-out group-hover:hidden"
-              />
-              <GiSurroundedEye
-                size={20}
-                color="black"
-                className="hidden transition-all duration-300 ease-in-out group-hover:block"
-              />
-              <div className="w-auto h-[100%] bg-green-500 justify-self-end self-end">
-                <p style={{ color: "white" }}>Menu</p>
-                <p style={{ color: "white" }}>Menu </p>
-              </div>
-            </div>
-          </button>
-        </div> */}
+   
 
         {/* TitleLogo */}
-        <div className="absolute bottom-0 w-3/4 transform -translate-x-1/2 left-1/2 font-fontBase bg-gsreen-600">
+        <div className="absolute w-3/4 transform -translate-x-1/2 top-1/2 md:bottom-0 left-1/2 font-fontBase bg-gsreen-600">
           <div className="absolute w-full h-auto transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
             <TitleLogo />
           </div>
