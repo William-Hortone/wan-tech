d663cbe9d06afcc66b9dad7532949cddcc30fea9
William Hortone <101454117+William-Hortone@users.noreply.github.com>
2025-02-19 03:18:21 +0800

    updated the portfolio  projects

--- src/components/CardImg.jsx
+++ src/components/CardImg.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const CardImg = ({ img, title, link }) => {
   return (
     <a href={link} target="blank" className="wrapperCardImg" >
-      <div className="w-[500px] h-[400px] bg-basic ">
+      <div className="w-[90vw] h-auto md:w-[500px] md:h-[400px] bg-basic ">
         <img className="w-[100%] h-[100%] cardImg" src={img} alt="project" />
         {/* <h4>{title}</h4> */}
       </div>
--- src/containers/projectsContainers/Portfolio.jsx
+++ src/containers/projectsContainers/Portfolio.jsx
@@ -1,64 +1,88 @@
-import React from "react";
-import HorizontalScroll from "react-scroll-horizontal";
+import React, { useRef } from "react";
 import { CardImg } from "../../components";
 import images from "../../constants/images";
-const Portfolio = () => {
-  const child = { width: `40em`, height: `100%` };
+import { FaCircleChevronLeft, FaCircleChevronRight } from "react-icons/fa6";
+const Projects = () => {
+  const scrollRef = useRef(null);
+
+  const scrollLeft = () => {
+    if (scrollRef.current) {
+      scrollRef.current.scrollBy({ left: -400, behavior: "smooth" });
+    }
+  };
+
+  const scrollRight = () => {
+    if (scrollRef.current) {
+      scrollRef.current.scrollBy({ left: 400, behavior: "smooth" });
+    }
+  };
+
+  const projectData = [
+    {
+      link: "https://gericht-a.netlify.app",
+      title: "Project 1",
+      img: images.img3,
+    },
+    {
+    
+      link:"https://fdp-app-client.vercel.app",
+      title:"project4",
+      img:images.img5
+    },
+    {
+      link:"https://onabatiment.com",
+      title: "project2",
+      img: images.img4,
+    },
+
+    {
+      link: "",
+      title: "Project 3",
+      img: images.project1,
+
+    },
+  ];
 
   return (
-    <>
-      <section className="portfolio-container w-full h-[70vh]">
-        <section className="portfolio-container w-[82%] h-[70vh]">
-          <HorizontalScroll>
+    <section className="h-[100vh] w-full  flex items-center justify-center">
+      <div className="flex relative gap-4 items-center bg-slate-400 justify-between py-4 w-full max-w-[100%]">
+        <button
+          onClick={scrollLeft}
+          className="absolute p-2 text-white bg-gray-500 rounded-full left-[45%] -bottom-10 hover:bg-gray-700"
+        >
+          <FaCircleChevronLeft size={22} color="#FF6F61" />
+        </button>
+
+        <div
+          ref={scrollRef}
+          className="flex w-full gap-3 py-4 space-x-4 overflow-x-hidden"
+        >
+          {projectData.map((project, index) => (
             <div
-              style={child}
-              className="flex items-center justify-start w-full "
+              key={index}
+              className="flex items-center justify-center flex-none bg-gray-200 rounded-lg shadow-lg"
             >
-              <div className="w-[100%] h-full bg-slate-500 flex items-center justify-start">
-                <CardImg
-                  link="https://gericht-a.netlify.app"
-                  title="project1"
-                  img={images.img3}
-                />
-              </div>
+              <CardImg
+                link={project.link}
+                title={project.title}
+                img={project.img}
+              />
             </div>
-            <div
-              style={child}
-              className="flex items-center justify-start w-full h-full"
-            >
-              <div className="w-[100%] h-full bg-slate-500 flex items-center justify-start">
-                <CardImg
-                  link="https://fdp-app-client.vercel.app"
-                  title="project4"
-                  img={images.img5}
-                />
-              </div>
-            </div>
-            <div
-              style={child}
-              className="flex items-center justify-start w-full h-full"
-            >
-              <div className="w-[100%] h-full bg-slate-500 flex items-center justify-start">
-                <CardImg
-                  link="https://onabatiment.com"
-                  title="project2"
-                  img={images.img4}
-                />
-              </div>
-            </div>
-            <div
-              style={child}
-              className="flex items-center justify-start w-full h-full"
-            >
-              <div className="w-[100%] h-full bg-slate-500 flex items-center justify-start">
-                <CardImg link="" title="project3" img={images.project1} />
-              </div>
-            </div>
-          </HorizontalScroll>
-        </section>
-      </section>
-    </>
+          ))}
+        </div>
+
+        <button
+          onClick={scrollRight}
+          className="absolute p-2 text-white bg-gray-500 rounded-full right-[45%] -bottom-10 hover:bg-gray-700"
+        >
+          <FaCircleChevronRight size={22} color="#FF6F61" />
+        </button>
+      </div>
+    </section>
   );
 };
 
-export default Portfolio;
+export default Projects;
+
+
+ 
